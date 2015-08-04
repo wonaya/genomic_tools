d068fae52eb9fe8d1811643240d3ec118ae4eb16
wonaya <jawon@tacc.utexas.edu>
2015-08-04 13:49:08 -0500

    fai_to_bed added

---
+++ fai_to_bed.py
@@ -0,0 +1,13 @@
+#### converts fai to bed 
+#### output chr	start	end	chrom_name
+
+import os, sys
+outfile = open(sys.argv[1].split(".fai")[0]+".bed",'w')
+for line in open(sys.argv[1], 'r') :
+    outfile.write(line.split("\t")[0])
+    outfile.write("\t")
+    outfile.write("1\t")
+    outfile.write(line.split("\t")[1])
+    outfile.write("\t")
+    outfile.write("chrom\n")
+outfile.close()
