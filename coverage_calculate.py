d6ea9da4f17e37a8af8bac25f1c734c07bc1af98
wonaya <jawon@tacc.utexas.edu>
2015-10-05 13:39:37 -0500

    calculate coverage

---
+++ coverage_calculate.py
@@ -0,0 +1,24 @@
+import os,sys
+### coverage calculator
+
+### usage coverage_calculate.py [bam] [fai] [-P] [read length]
+
+### get genome size
+
+genome_size = 0
+for fai in open(sys.argv[2], 'r') :
+    genome_size += int(fai.split("\t")[1])
+
+os.system("samtools flagstat "+sys.argv[1]+" > flagstat.out")
+flagstat = open("flagstat.out", 'r') 
+a = flagstat.readlines()
+mapped_reads = a[2].split(" + ")[0]
+if sys.argv[3] == "-P" :
+    total_base = int(mapped_reads)*2*int(sys.argv[4])
+else :
+    total_base = int(mapped_reads)*int(sys.argv[4])
+coverage = float(total_base)/float(genome_size)
+
+print coverage
+    
+    
