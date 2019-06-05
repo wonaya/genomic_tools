ad79bb14c98484a9886c7a18f4801e47f563d507
Jawon Song <jawon@tacc.utexas.edu>
2019-06-05 10:18:00 -0400

    Update 1xnorm.py

--- 1xnorm.py
+++ 1xnorm.py
@@ -1,4 +1,5 @@
-#Usage python 1xnorm.py file.txt reffile.txt prefix
+#Updated 6/5/19
+#USAGE python 1xnorm.py file.txt reffile.txt fai windowsize prefix
 #file.txt contains list of test .bam files e.g. ES1.bam\nES2.bam\nES3.bam
 #reffile.txt contains list of reference .bam files e.g. control1.bam\ncontrol2.bam
 
@@ -11,15 +12,26 @@ for a in open(sys.argv[1], 'r'):
 for a in open(sys.argv[2], 'r') :
     reffile.append(a.strip("\n"))
 
+### make genome windows
+window = open("tmp.window", 'w')
+for a in open(sys.argv[3], 'r') :
+    start = 0
+    for x in range(0, int(float(a.split("\t")[1])/float(sys.argv[4]))) :
+        window.write(a.split("\t")[0]+"\t")
+        window.write(str(x*float(sys.argv[4]))+"\t")
+        window.write(str((x+1)*float(sys.argv[4]))+"\n")
+    window.write(a.split("\t")[0]+"\t"+str((x+1)*float(sys.argv[4]))+"\t"+a.split("\t")[1]+"\n")
+window.close()
+
 ### extract name of each sample from file.txt file names
 readlen = []
 for files in file :
     name=files.split(".bam")[0]
-    #print files
+    print files
     # run bed tools to convert bam file to bed file and extract columns and sort
     os.system("export LC_ALL=C; bedtools bamtobed -i %s | cut -f 1-3 | sort -S 1G -k1,1 -k2,2n > %s"%(files, name+".bed"))
     # overlap with maize genome interval to output to file chr1\n1\1000\nES etc. 
-    os.system("bedtools intersect -a %s -b %s -c -sorted > %s"%("maize_4.33_w1000.bed", name+".bed", name+".bedgraph"))
+    os.system("bedtools intersect -a %s -b %s -c -sorted > %s"%("tmp.window", name+".bed", name+".bedgraph"))
     outfile = open(name+"_1xnorm.bedgraph", 'w')
     from collections import defaultdict 
     from operator import itemgetter
