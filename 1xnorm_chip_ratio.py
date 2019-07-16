db28605584df78906f13cd50069e0c11aa16a8b1
Jawon Song <jawon@tacc.utexas.edu>
2019-07-16 12:18:09 -0400

    Update 1xnorm_chip_ratio.py

--- 1xnorm_chip_ratio.py
+++ 1xnorm_chip_ratio.py
@@ -1,4 +1,5 @@
 import os,sys
+import datetime
 #python 1xnorm.py file.txt reffile.txt windowfile prefix readlength fai
 
 readlength=int(sys.argv[5])
@@ -11,18 +12,20 @@ for a in open(sys.argv[1], 'r'):
     file.append(a.strip("\n"))
 for a in open(sys.argv[2], 'r') :
     reffile.append(a.strip("\n"))
-
+print "START: "+str((datetime.datetime.now()))
+print "##### RUNNING TEST FILES #####"
 readlen = []
 for files in file :
     name=files.split(".bam")[0]
-    print files
+    print "#####"+str(files)+"#####"
+    print "##### RUNNING BEDTOOLS #####"
     os.system("export LC_ALL=C; bedtools bamtobed -i %s | cut -f 1-3 | sort -S 1G -k1,1 -k2,2n > %s"%(files, name+".bed"))
     os.system("sortBed -i "+name+".bed -faidx "+sys.argv[6]+" > tmp.bed")
     os.system("mv tmp.bed "+name+".bed")
 
     ###make into windowsize tiles 
     os.system("bedtools intersect -a %s -b %s -c -sorted > %s"%(windowfile, name+".bed", name+".bedgraph"))
-    
+    print "##### NORMALIZING #####"
     outfile = open(name+"_1xnorm.bedgraph", 'w')
     from collections import defaultdict 
     from operator import itemgetter
@@ -39,7 +42,7 @@ for files in file :
     for a in open(name+".bedgraph", 'r') :
         total_reads += float(a.split("\t")[-1].strip("\n"))
     scale = 1/(total_reads*readlength/2.3e9)
-    print scale
+    print "##### SCALE FACTOR:"+scale+"#####"
     # Process each chrom to 1X
     sChroms = sorted(genome.keys())
     for chrom in sChroms:
@@ -51,16 +54,19 @@ for files in file :
 	    else:
 		outfile.write('%s\t%i\t%i\t%.3f\n'%(chrom, s, e, oV))
     outfile.close()
-  
+
+print "##### RUNNING REF FILES #####"
 ## do the same for input file
 for files in reffile :
     name=files.split(".bam")[0]
-    print files
+    print "#####"+files+"#####"
+    print "##### RUNNING BEDTOOLS #####"
     os.system("export LC_ALL=C; bedtools bamtobed -i %s | cut -f 1-3 | sort -S 1G -k1,1 -k2,2n > %s"%(files, name+".bed"))
     os.system("sortBed -i "+name+".bed -faidx "+sys.argv[6]+" > tmp.bed")
     os.system("mv tmp.bed "+name+".bed")
     os.system("bedtools intersect -a %s -b %s -c -sorted > %s"%(windowfile, name+".bed", name+".bedgraph"))
     outfile = open(name+"_1xnorm.bedgraph", 'w')
+    print "##### NORMALIZING #####"
     from collections import defaultdict
     from operator import itemgetter
     BGF = open(name+".bedgraph", 'r')
@@ -77,7 +83,7 @@ for files in reffile :
         total_reads += float(a.split("\t")[-1].strip("\n"))
     #print total_reads
     scale = 1/(total_reads*readlength/2.3e9)
-    print scale
+    print "##### SCALE FACTOR:"+scale+"#####"
     # Process each chrom
     sChroms = sorted(genome.keys())
     for chrom in sChroms:
@@ -90,9 +96,9 @@ for files in reffile :
                 outfile.write('%s\t%i\t%i\t%.3f\n'%(chrom, s, e, oV))
     outfile.close()
 
-print "### intersectBed DONE###"
 os.system("rm -Rf tmp*bed")
 
+print "##### MERGING REPLICATES #####"
 filR = open(windowfile, 'r')
 linR = filR.readlines()
 collect = []
@@ -107,7 +113,7 @@ for fil in reffile :
     lines = file_content.readlines()
     ref_collect.append(lines)
 
-outfile = open(str(prefix)+"_merged.bedgraph", 'w')
+outfile = open(str(prefix)+"_merge.bedgraph", 'w')
 index = 0
 for lineR in linR : 
     sum = 0
@@ -131,11 +137,75 @@ for lineR in linR :
     index += 1
 outfile.close()
 
-### divide it
-outfile = open("ratio_1x_"+str(prefix)+".bedgraph", 'w')
+print "##### SCALE MERGED FILE TO 1X #####"
+### scale to 1x merged again
+outfile = open(str(prefix)+"_merge_1xnorm.bedgraph", 'w')
+from collections import defaultdict
+from operator import itemgetter
+BGF = open(str(prefix)+"_merge.bedgraph", 'r')
+noComments = filter(lambda x: x[0] != '#', BGF)
+genome = defaultdict(list)
+# Read Data
+for line in noComments:
+    tmp = line.rstrip('\n').split('\t')
+    s, e, v = int(tmp[1]), int(tmp[2]), float(tmp[3])
+    genome[tmp[0]].append((s,e,v))
+total_reads = 0
+for a in open(str(prefix)+"_merge.bedgraph", 'r') :
+    total_reads += float(a.split("\t")[-1].strip("\n"))
+scale = 1/(total_reads*readlength/2.3e9)
+print "##### SCALE FACTOR:"+scale+"#####"
+# Process each chrom
+sChroms = sorted(genome.keys())
+for chrom in sChroms:
+    for record in genome[chrom]:
+        s, e, v = record
+        oV = v*scale
+        if int(oV) == oV:
+            outfile.write('%s\t%i\t%i\t%i\n'%(chrom, s, e, oV))
+        else:
+            outfile.write('%s\t%i\t%i\t%.3f\n'%(chrom, s, e, oV))
+outfile.close()
 
-mergef = open(str(prefix)+"_merged.bedgraph",'r')
-merger = open(str(prefix)+"_mergeref.bedgraph", 'r')
+print "##### SCALE MERGED REF FILE TO 1X #####"
+### scale to 1x for merged ref
+outfile = open(str(prefix)+"_mergeref_1xnorm.bedgraph", 'w')
+from collections import defaultdict
+from operator import itemgetter
+BGF = open(str(prefix)+"_mergeref.bedgraph", 'r')
+noComments = filter(lambda x: x[0] != '#', BGF)
+genome = defaultdict(list)
+# Read Data
+for line in noComments:
+    tmp = line.rstrip('\n').split('\t')
+    s, e, v = int(tmp[1]), int(tmp[2]), float(tmp[3])
+    genome[tmp[0]].append((s,e,v))
+## Calculate scaling factor and normalize
+total_reads = 0
+for a in open(str(prefix)+"_mergeref.bedgraph", 'r') :
+    total_reads += float(a.split("\t")[-1].strip("\n"))
+#print total_reads
+scale = 1/(total_reads*readlength/2.3e9)
+print "##### SCALE FACTOR:"+scale+"#####"
+# Process each chrom
+sChroms = sorted(genome.keys())
+for chrom in sChroms:
+    for record in genome[chrom]:
+        s, e, v = record
+        oV = v*scale
+        if int(oV) == oV:
+            outfile.write('%s\t%i\t%i\t%i\n'%(chrom, s, e, oV))
+        else:
+            outfile.write('%s\t%i\t%i\t%.3f\n'%(chrom, s, e, oV))
+outfile.close()
+
+print "##### CALCULATING RATIO #####"
+
+### divide it
+outfile = open("ratio_merge_1x_"+str(prefix)+".bedgraph", 'w')
+
+mergef = open(str(prefix)+"_merge_1xnorm.bedgraph",'r')
+merger = open(str(prefix)+"_mergeref_1xnorm.bedgraph", 'r')
 mergel=mergef.readlines()
 mergerl=merger.readlines()
 index = 0
@@ -159,4 +229,5 @@ outfile.close()
 #    else :
 #        outfile.write("0\n")
 #outfile.close()
-os.system("rm -Rf "+str(prefix)+"_merged.bedgraph "+str(prefix)+"_mergeref.bedgraph")
+#os.system("rm -Rf "+str(prefix)+"_merged.bedgraph "+str(prefix)+"_mergeref.bedgraph")
+print "FINISH: "+str((datetime.datetime.now()))
